1905afb496cd02845d9f62b0dd2ba53ebd943e81
Kyle Blake <kyle.blake84@gmail.com>
2017-03-23 20:40:34 -0500

    2nd interation: Cut down one check.

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -4,5 +4,4 @@ isLeapYear :: Integer -> Bool
 isLeapYear year
     | year `mod` 400 == 0 = True
     | year `mod` 100 == 0 = False
-    | year `mod` 4 == 0   = True
-    | otherwise           = False
+    | otherwise           = year `mod` 4 == 0
